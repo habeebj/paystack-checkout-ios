28daf2f367ae1e2fecca452f96ed94d0bbfdf7a9
Jimoh Habeeb <habeebjimoh@hotmail.com>
2021-01-24 19:17:29 +0100

    Added public contructor to metadata and customFilters

--- Sources/PaystackCheckout/Classes/Metadata.swift
+++ Sources/PaystackCheckout/Classes/Metadata.swift
@@ -12,25 +12,35 @@ public struct CustomFilters: Encodable {
     let recurring: Bool
     let banks: [String]
     let card_brands: [String]
+
+    public init(recurring: Bool, banks: [String], cardBrands: [String]) {
+        self.recurring = recurring
+        self.banks = banks
+        self.card_brands = cardBrands
+    }
 }
 
 public struct Metadata: Encodable {
-    let cancel_action: String
-    let custom_filters: CustomFilters
+    private var custom_filters: CustomFilters? = nil
     let custom_fields: [CustomField]
+
+    public init(customFields: [CustomField] = [], customFilters: CustomFilters? = nil) {
+        self.custom_fields = customFields
+        self.custom_filters = customFilters
+    }
 }
 
 public struct CustomField: Encodable {
     let display_name: String
     let variable_name: String
     let value: String
-    
+
     public init(displaName: String, variableName: String, value: String) {
         self.display_name = displaName
         self.variable_name = variableName
         self.value = value
     }
-    
+
     public init(variableName: String, value: String) {
         self.display_name = variableName
         self.variable_name = variableName
